c4f8cb2305748f099d51b6c2ec459a2b2d0af972
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2022-05-23 22:14:10 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeTrainingsMonitoring.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
